240bd0f9bf90201d506fe6e448053d7d40a84ee5
anyable <stupiranna@mail.ru>
2025-12-01 16:59:48 +0300

    IDraw

--- main.cpp
+++ main.cpp
@@ -1,2 +1,22 @@
-int main() 
-{}
+namespace top
+{
+    struct IDraw {
+    virtual p_t begin() = 0;
+    virtual p_t next(p_t) = 0;
+  };
+
+  bool operator==(p_t a, p_t b) {
+    return a.x == b.x && a.y == b.y;
+  }
+  bool operator !=(p_t a, p_t b) {
+    return !(a == b);
+  }
+  struct p_t {
+    int x, y;
+  };
+}
+
+int main() {
+  using namespace top;
+}
+
