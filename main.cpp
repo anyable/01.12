285bc40a0ef859fb1b99524dae2736f898009d69
anyable <stupiranna@mail.ru>
2025-12-01 17:49:08 +0300

    main

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 namespace top
 {
     struct IDraw {
@@ -30,9 +31,42 @@ namespace top
   top::p_t top::Dot::next(p_t) const {
     return begin();
   }
+  struct frame_t {
+    p_t left_bot;
+    p_t right_top;
+  };
+  void make_f(IDraw ** b, size_t k);
+  void get_points(IDraw * b, p_t ** ps, size_t & s); //будет расширять массив точек 
+  frame_t build_frame(const p_t * ps, size_t s);
+  char * build_canvas(frame_t f);
+  void paint_canvas(char * cnv, frame_t r, const p_t * ps, size_t k, char f); //перевести координаты точек в канвас 
+  void print_canvas(const char * cnv, frame_t fr);
 }
+
 
 int main() {
   using namespace top;
+  IDraw  * f[3] = {}; // массив фигур 
+  char * can = nullptr; //канвас 
+  p_t * p = nullptr; 
+  size_t s = 0;
+  try {
+    make_f(f, 3);
+    for (size_t i = 0; i < 3; ++i) {
+      get_points(f[i], &p, s);
+    }
+    frame_t fo = build_frame(p, s);
+    can = build_canvas(fo);
+    paint_canvas(can, fo, p, s, '*');
+    print_canvas(can, fo);
+  } catch (...) {
+    bool err = 1;
+  }
+  delete f[0];
+  delete f[1];
+  delete f[2];
+  delete [] p;
+  delete [] can;
+  return err;
 }
 
