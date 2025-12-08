543cb677f8b4c82abf43ea4db5acfc8dd5a08982
anyable <stupiranna@mail.ru>
2025-12-08 22:17:44 +0300

    correct naming and compilation

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 namespace top
 {
-
-
-  struct p_t
+  struct p_t //точка
   {
     int x, y;
   };
 
-  struct IDraw //абстрактный класс
+  struct IDraw //абстрактный класс, для рисования
   {
+    virtual ~IDraw() = default;
     virtual p_t begin() const = 0;
     virtual p_t next(p_t) const = 0;
   };
@@ -24,17 +23,18 @@ namespace top
     return !(a == b);
   }
 
-  struct Dot: IDraw {
+  struct Dot: IDraw { //точка для рисования
     p_t begin() const override; //центр фигуры
     p_t next(p_t) const override; //следущая точка
     p_t o;
-    Dot(int x, int y);
+    explicit Dot(p_t dd); //конструктор из точки
+    Dot(int x, int y); //конструктор из 2 координат
   };
 
   struct frame_t
   {
-    p_t left_bot; //левая граница
-    p_t right_top; //правая граница
+    p_t aa; //левая граница
+    p_t bb; //правая граница
   };
 
   struct VSeg
@@ -51,16 +51,21 @@ namespace top
     int right_x;
   };
 
-  void make_f(IDraw ** b, size_t k); //выделяем память под фигуры и создаем точки через конструктор
-  void get_points(IDraw * b, p_t ** ps, size_t & s); //будет расширять массив точек
+  void make_f(IDraw ** b, size_t k); //выделяем память под фигуры и создаем точки через конструктор, то есть центры
+
+  void get_points(const IDraw & d, p_t ** pts, size_t & s); //будет расширять массив точек
+
   frame_t build_frame(const p_t * ps, size_t s); // макс и мин точки под рамку
-  char * build_canvas(frame_t f); //выделяем память под канвас
-  void paint_canvas(char * cnv, frame_t r, const p_t * ps, size_t k, char f); //перевести координаты точек в канвас
-  void print_canvas(const char * cnv, frame_t fr); //вывод канваса, то есть двумерной матрицы
-  void extend(p_t ** pts, size_t s, p_t p);
-  size_t points(const IDraw & d, p_t ** res, size_t s);
+
+  char * build_canvas(frame_t f, char fill); //выделяем память под канвас
+
+  void paint_canvas(char * cnv, frame_t fr, p_t p, char fill); //перевести координаты точек в канвас
+
+  void print_canvas(std::ostream & os, const char * cnv, frame_t fr); //вывод канваса, то есть двумерной матрицы
+
+  void extend(p_t ** pts, size_t s, p_t p); //расширение массива 
 }
-top::Dot::Dot(int x, int y): // конструктор создает центр?
+top::Dot::Dot(int x, int y): // конструктор создает центр
     IDraw(),
     o{x, y}
     {}
@@ -82,53 +87,6 @@ void top::make_f(IDraw ** b, size_t k)
   b[2] = new Dot(7, 7);
 }
 
-void top::get_points(IDraw * b, p_t ** ps, size_t & s)
-{
-  while (b)
-  {
-    p_t ** new_ps = new p_t * [++s];
-    for (size_t i = 0; i < s - 1; ++i)
-    {
-      new_ps[i] = ps[i];
-    }
-    new_ps[s - 1] = &(b -> begin());
-    delete [] ps;
-    ps = new_ps;
-    b++;
-  }
-}
-
-int main()
-{
-  using namespace top;
-  int err = 0;
-  IDraw  * f[3] = {}; // массив фигур
-  char * can = nullptr; //канвас на котором будут фигуры
-  p_t * p = nullptr; //массив из цетров фигур
-  size_t s = 0;
-  try
-  {
-    make_f(f, 3);
-    for (size_t i = 0; i < 3; ++i)
-    {
-      get_points(f[i], &p, s);
-    }
-    frame_t fo = build_frame(p, s); //рамка
-    can = build_canvas(fo); //канвас
-    paint_canvas(can, fo, p, s, '*');
-    print_canvas(can, fo);
-  }
-  catch (...)
-  {
-    err = 1;
-  }
-  delete f[0];
-  delete f[1];
-  delete f[2];
-  delete [] p;
-  delete [] can;
-  return err;
-}
 void top::extend(p_t ** pts, size_t s, p_t p)
 {
   p_t * res = new p_t[s + 1];
@@ -141,10 +99,9 @@ void top::extend(p_t ** pts, size_t s, p_t p)
   *pts = res;
 }
 
-
-size_t top::points(const IDraw & d, p_t ** res, size_t s)
+void top::get_points(const IDraw & d, p_t ** pts, size_t & s)
 {
-  p_t p = d.begin();
+  p_t p = d.begin(); //центр фигуры
   extend(pts, s, p);
   size_t delta = 1;
   while (d.next(p) != d.begin())
@@ -154,5 +111,112 @@ size_t top::points(const IDraw & d, p_t ** res, size_t s)
     ++delta;
     // p добавить в массив
   }
-  return delta;
+  s += delta;
 }
+
+size_t rows(top::frame_t fr)
+{
+  return (fr.bb.y - fr.aa.y + 1);
+}
+
+size_t cols(top::frame_t fr)
+{
+  return (fr.bb.x - fr.aa.x + 1);
+}
+
+top::frame_t top::build_frame(const p_t * pts, size_t s)
+{
+  if (!s)
+  {
+    throw std::logic_error("bad size");
+  }
+  int minx = pts[0].x, maxx = minx;
+  int miny = pts[0].y, maxy = miny;
+  for (size_t i = 1; i < s; ++i)
+  {
+    minx = std::min(minx, pts[i].x);
+    maxx = std::max(maxx, pts[i].x);
+    miny = std::min(miny, pts[i].y);
+    maxy = std::max(maxy, pts[i].y);
+  }
+  p_t aa{minx, miny};
+  p_t bb{maxx, maxy};
+  return {aa, bb};
+}
+
+char * top::build_canvas(frame_t fr, char fill)
+{
+  char * cnv = new char[rows(fr) * cols(fr)];
+  for (size_t i = 0; i < rows(fr) * cols(fr); ++i)
+  {
+    cnv[i] = fill;
+  }
+  return cnv;
+}
+
+void top::paint_canvas(char * cnv, frame_t fr, p_t p, char fill)
+{
+  int dx = p.x - fr.aa.x;
+  int dy = fr.bb.y - p.y;
+  cnv[dy * cols(fr) + dx] = fill;
+}
+
+void top::print_canvas(std::ostream & os, const char * cnv, frame_t fr)
+{
+  for (size_t i = 0; i < rows(fr); ++i)
+  {
+    for (size_t j = 0; j < cols(fr); ++j)
+    {
+      os << cnv[i * cols(fr) + j];
+    }
+    os << '\n';
+  }
+}
+
+top::Dot::Dot(p_t dd):
+ IDraw(),
+ o{dd}
+{}
+
+int main()
+{
+  using namespace top;
+  int err = 0; //индиф ошибки
+  IDraw  * f[3] = {}; // массив фигур
+  char * can = nullptr; //канвас на котором будут фигуры
+  p_t * p = nullptr; //массив из цетров фигур
+  size_t s = 0;
+  try
+  {
+    make_f(f, 3);
+    for (size_t i = 0; i < 3; ++i)
+    {
+      get_points(*(f[i]), &p, s);
+    }
+    frame_t fo = build_frame(p, s); //рамка
+    can = build_canvas(fo, '*'); //канвас
+    for (size_t i = 0; i < s; ++i)
+    {
+      paint_canvas(can, fo, p[i], '.');
+    }
+    print_canvas(std::cout, can, fo);
+  }
+  catch (...)
+  {
+    err = 1;
+  }
+  delete f[0];
+  delete f[1];
+  delete f[2];
+  delete [] p;
+  delete [] can;
+  return err;
+}
+
+
+
+
+
+
+
+
